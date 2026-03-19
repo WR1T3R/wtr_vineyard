9b719c08b452a7b231bfa173ea719fe2c6ec3e86
W R I T E R <alexisl231@outlook.com>
2026-03-19 00:12:44 -0400

    Add images + items

---
+++ info/items.lua
@@ -0,0 +1,89 @@
+["red_wine_bottle"] = {
+    label = "Bouteille de vin rouge",
+    weight = 10
+},
+
+["red_wine_glass"] = {
+    label = "Verre de vin rouge",
+    weight = 10
+},
+
+["red_wine_grapes"] = {
+    label = "Raisins rouges",
+    weight = 10
+},
+
+["red_wine_juice"] = {
+    label = "Jus de raisins rouges",
+    weight = 10
+},
+
+["red_wine_prepared"] = {
+    label = "Raisins rouges préparés",
+    weight = 10
+},
+
+["rose_wine_bottle"] = {
+    label = "Bouteille de vin rose",
+    weight = 10
+},
+
+["rose_wine_glass"] = {
+    label = "Verre de vin rose",
+    weight = 10
+},
+
+["rose_wine_grapes"] = {
+    label = "Raisins roses",
+    weight = 10
+},
+
+["rose_wine_juice"] = {
+    label = "Jus de raisins roses",
+    weight = 10
+},
+
+["rose_wine_prepared"] = {
+    label = "Raisins roses préparés",
+    weight = 10
+},
+
+["white_wine_bottle"] = {
+    label = "Bouteille de vin blanc",
+    weight = 10
+},
+
+["white_wine_glass"] = {
+    label = "Verre de vin blanc",
+    weight = 10
+},
+
+["white_wine_grapes"] = {
+    label = "Raisins blancs",
+    weight = 10
+},
+
+["white_wine_juice"] = {
+    label = "Jus de raisins blancs",
+    weight = 10
+},
+
+["white_wine_prepared"] = {
+    label = "Raisins blancs préparés",
+    weight = 10
+},
+
+["scissors"] = {
+    label = "Ciseaux",
+    weight = 10
+},
+
+["empty_wine_bottle"] = {
+    label = "Bouteille de vin vide",
+    weight = 10
+},
+
+["wine_label"] = {
+    label = "Étiquette de vin",
+    weight = 10
+}
